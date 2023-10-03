3dd431b40e0bf017880b08061396d9e565ac1f22
JonatanLinberg <98joli@gmail.com>
2023-10-03 21:20:19 +0200

    Started implementing app interface

---
+++ CLIApp.py
@@ -0,0 +1,10 @@
+
+def print_welcome():
+	print("================================\n" + \
+		  "==   Welcome to FundamenTUNE  ==\n" + \
+		  "================================\n")
+
+def main(*args, **kwargs):
+	print_welcome()
+	print("As of now there is no app, sorry.\n" + \
+		  "Feel free to use the code as a library though! :)\n")
--- FundamenTUNE.py
+++ FundamenTUNE.py
@@ -1,1 +1,8 @@
-	
+from interval import Interval
+from CLIApp import main as CLI_main
+
+if __name__ == '__main__':
+	no_gui = True
+
+	if no_gui:
+		CLI_main()
